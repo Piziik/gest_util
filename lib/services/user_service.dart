04707e3865097f81bed1576c0527900e028ae245
Yanis OULTAF <yanis.oultaf@gmail.com>
2024-06-12 15:41:10 +0200

    fix: null string issue

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,7 +1,5 @@
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:universal_io/io.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import '../models/user.dart';
 
 class UserService {
@@ -12,20 +10,11 @@ class UserService {
   }
 
   Future<Isar> _initDb() async {
-    if (kIsWeb) {
-      // Pour le Web, utilisez un chemin par défaut
-      return await Isar.open(
-        [UserSchema],
-        directory: null, // Le Web n'a pas besoin de directory
-      );
-    } else {
-      // Pour les plateformes non-Web (Android, iOS, macOS, Linux, Windows)
-      final dir = await getApplicationDocumentsDirectory();
-      return await Isar.open(
-        [UserSchema],
-        directory: dir.path,
-      );
-    }
+    final dir = await getApplicationDocumentsDirectory();
+    return await Isar.open(
+      [UserSchema],
+      directory: dir.path,
+    );
   }
 
   Future<List<User>> getUsers() async {
